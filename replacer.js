c2450279d0c19846298beaa4f474b019f041deeb
Jennifer Wu <jenniferyhwu@gmail.com>
2019-03-10 12:11:02 -0400

    fixed stuff

--- replacer.js
+++ replacer.js
@@ -1,23 +1,24 @@
 chrome.storage.sync.get('name', function(data) {
     if (data.name == "Kevin Cong Pei") {
         alert(data.name);
-        var allImages = document.getElementsByTagName('img');
-
-        for (var i = 0; i < allImages.length; i++) {
-            allImages[i].addEventListener('mouseover', function() {
-                var thisImageHeight = this.clientHeight;
-                var thisImageWidth = this.clientWidth;
-
-                this.setAttribute('src', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth)
-            })
-        }
+        replace();
     } else {
         alert("WRONG PERSON ABORT ABORT");
     }
 });
 
+function replace() {
+    var allImages = document.getElementsByTagName('img');
+
+    for (var i = 0; i < allImages.length; i++) {
+        var thisImageHeight = allImages[i].clientHeight;
+        var thisImageWidth = allImages[i].clientWidth;
+        allImages[i].setAttribute('src', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth)
+    }
+}
+
  // Select the node that will be observed for mutations
- var targetNode = document.getElementsByTagName('img');
+ var targetNode = document.querySelector('body');
 
  // Options for the observer (which mutations to observe)
  var config = { attributes: true, childList: true, subtree: true };
@@ -25,13 +26,30 @@ chrome.storage.sync.get('name', function(data) {
  // Callback function to execute when mutations are observed
  var callback = function(mutationsList, observer) {
      for(var mutation of mutationsList) {
-         if (mutation.type == 'childList') {
-             console.log('A child node has been added or removed.');
-         }
-         else if (mutation.type == 'attributes') {
-             console.log('The ' + mutation.attributeName + ' attribute was modified.');
-         }
-     }
+        //  if (mutation.type == 'childList') {
+        //      console.log('A child node has been added or removed.');
+        //      let changed_values = [].slice.call(targetNode.children)
+        //         .map( function(node) { return node.attribute; })
+        //         .filter( function(s) {
+        //         if (s === '<br />') {
+        //             return false;
+        //         }
+        //     });
+        //     console.log('mutations are: ' + changed_values);
+        //  }
+        //  else 
+        if (mutation.type == 'attributes') {
+            console.log('The ' + mutation.attributeName + ' attribute was modified.');
+            let changed_values = [].slice.call(targetNode.children)
+                .map( function(node) { return node.attribute; })
+                .filter( function(s) {
+                if (s === '<br />') {
+                    return false;
+                }
+            });
+            console.log('modifications are: ' + changed_values);  
+        }
+    }
  };
 
  // Create an observer instance linked to the callback function
