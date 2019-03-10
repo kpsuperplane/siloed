f3c13571634738318e466f56cc77cc483cab0faf
Jennifer Wu <jenniferyhwu@gmail.com>
2019-03-10 16:34:43 -0400

    fixed

--- replacer.js
+++ replacer.js
@@ -1,42 +1,47 @@
-chrome.storage.sync.get('name', function(data) {
-    if (data.name == "Kevin Cong Pei") {
-        alert(data.name);
-        var allImages = document.getElementsByTagName('img');
+function replaceAllInitial() {
+    var allImages = document.getElementsByTagName('img');
+    chrome.storage.sync.get('scale', function(data) {
         for (var i = 0; i < allImages.length; i++) {  
-            replaceOne(allImages[i]);
+            replaceOne(allImages[i], data.scale);
         }
-    } else {
-        alert("WRONG PERSON ABORT ABORT");
-    }
-});
+    });
+}
 
-function replaceUPDATE(image) {
-    if(image.src != undefined && image.src.indexOf('placedog') != -1) 
-    return;
-    
-    if(image instanceof HTMLImageElement){
-        var thisImageHeight = image.clientHeight;
-        var thisImageWidth = image.clientWidth;
-        console.log("reached replace one insideinside");
-        image.setAttribute('src', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth)
-        image.setAttribute('srcset', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth)
+// changes image edits according to slider info here
+function replaceImage(image, scale, thisImageHeight, thisImageWidth) {
+    if (scale == '100') {
+        image.setAttribute('src', 'https://placedog.net/' + thisImageWidth + '/' + thisImageHeight);
+        image.setAttribute('srcset', 'https://placedog.net/' + thisImageWidth + '/' + thisImageHeight);
     }
 }
 
-function replaceOne(image) {
-    if(image.src != undefined && image.src.indexOf('placedog') != -1) 
+function replaceUPDATE(image, scale) {
+    if (image.src != undefined && image.src.indexOf('placedog') != -1) 
     return;
     
-    if(image instanceof HTMLImageElement){
+    if (image instanceof HTMLImageElement){
         var thisImageHeight = image.clientHeight;
         var thisImageWidth = image.clientWidth;
         console.log("reached replace one insideinside");
-        if (thisImageHeight != 0 && thisImageHeight != 0){
-            image.setAttribute('src', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth)
-            image.setAttribute('srcset', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth)
+        replaceImage(image, scale, thisImageHeight, thisImageWidth);
+    }
+}
+
+function replaceOne(image, scale) {
+    if (image.src != undefined && image.src.indexOf('placedog') != -1) 
+    return;
+    
+    if (image instanceof HTMLImageElement){
+        var thisImageHeight = image.clientHeight;
+        var thisImageWidth = image.clientWidth;
+        console.log("reached replace one insideinside");
+        if (thisImageHeight != 0 && thisImageHeight != 0) {
+            replaceImage(image, scale, thisImageHeight, thisImageWidth);
         }
     }
 }
+
+replaceAllInitial();
 
 // Select the node that will be observed for mutations
 var targetNode = document.querySelector('body');
@@ -46,17 +51,21 @@ var config = { attributes: true, childList: true, subtree: true };
 
 // Callback function to execute when mutations are observed
 var callback = function(mutationsList, observer) {
-    //replace(mutationsList);
-    for(var mutation of mutationsList) {
-        if (mutation.type == 'attributes') {
-            //console.log('The ' + mutation.attributeName + ' attribute was modified.');
-            if (mutation.attributeName == 'src') {
-                replaceUPDATE(mutation.target);
+    chrome.storage.sync.get('scale', function(data) {
+        let scale = data.scale;
+        //alert(scale);
+        //replace(mutationsList);
+        for (var mutation of mutationsList) {
+            if (mutation.type == 'attributes') {
+                //console.log('The ' + mutation.attributeName + ' attribute was modified.');
+                if (mutation.attributeName == 'src') {
+                    replaceUPDATE(mutation.target, scale);
+                }
             }
+            
+            replaceOne(mutation.target, scale);
         }
-        
-        replaceOne(mutation.target);
-    }
+    });
 };
 
 // Create an observer instance linked to the callback function
