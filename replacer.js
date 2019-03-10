9bc97103d7943f10597595e92d3513ec66afef6d
Jennifer Wu <jenniferyhwu@gmail.com>
2019-03-10 14:31:38 -0400

    cleaned stuff up

--- replacer.js
+++ replacer.js
@@ -1,31 +1,12 @@
-// replaces all images at start
 chrome.storage.sync.get('name', function(data) {
     if (data.name == "Kevin Cong Pei") {
         alert(data.name);
-        var allImages = document.getElementsByTagName('img');
-        for (var i = 0; i < allImages.length; i++) {  
-            replace(allImages[i]);
-        }
+        replace(document);
     } else {
         alert("WRONG PERSON ABORT ABORT");
     }
 });
 
-// used to replace all images at start
-function replace(image) {
-    console.log("will mutate: " + image.src);   
-
-    var thisImageHeight = image.clientHeight;
-    var thisImageWidth = image.clientWidth; 
-    
-    // image.classList.add('we-already-changed-this');
-    image.setAttribute('src', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth);
-    image.setAttribute('srcset', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth);
-
-    console.log("done changing: " + image.src);
-}
-
-// used to replace updated images
 function replaceOne(image) {
     image = image.target;
 
@@ -37,13 +18,27 @@ function replaceOne(image) {
         var thisImageWidth = image.clientWidth;
         console.log("reached replace one insideinside");
         if(thisImageHeight != 0 && thisImageHeight != 0){
-            // image.classList.add('we-already-changed-this');
             image.setAttribute('src', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth)
             image.setAttribute('srcset', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth)
         }
     }
 
 }
+
+function replaceUPDATED(image) {
+    //console.log("will mutate: " + image.src);   
+
+    var thisImageHeight = image.clientHeight;
+    var thisImageWidth = image.clientWidth;
+    //if (!(thisImageHeight == 0 || thisImageWidth == 0)) {  
+        image.classList.add('we-already-changed-this');
+        image.setAttribute('src', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth);
+        image.setAttribute('srcset', 'https://placedog.net/' + thisImageHeight + '/' + thisImageWidth);
+    //}
+
+    //console.log("done changing: " + image.src);
+}
+
 
 // Select the node that will be observed for mutations
 var targetNode = document.querySelector('body');
@@ -55,9 +50,16 @@ var config = { attributes: true, childList: true, subtree: true };
 var callback = function(mutationsList, observer) {
     //replace(mutationsList);
     for(var mutation of mutationsList) {
-        //if (!mutation.target.classList.contains('we-already-changed-this')) {
-            replaceOne(mutation);
-        //}
+        if (mutation.type == 'attributes') {
+            //console.log('The ' + mutation.attributeName + ' attribute was modified.');
+            if (mutation.attributeName == 'src') {
+                if (!mutation.target.classList.contains('we-already-changed-this')) {
+                    replaceUPDATED(mutation.target);
+                }
+            }
+        }
+        
+        replaceOne(mutation);
     }
 };
 
